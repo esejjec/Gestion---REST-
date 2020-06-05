12fff54d4bfe0ea11d675993d29dcfc445f4a89c
Erika Sejje <esejjec@unsa.edu.pe>
2020-06-05 09:57:37 -0500

    bd

--- conexion/Config.php
+++ conexion/Config.php
@@ -7,7 +7,7 @@
 define("DB_HOST", "127.0.0.1");
 define("DB_USER", "root");
 define("DB_PASSWORD", "gestion5");
-define("DB_DATABASE", "rest_api");
+define("DB_DATABASE", "gestion_REST");
 define("ROOT_URL", "http://localhost/gestion");
 define("XML_FILE", "rest/data_xml.php");
 define("JSON_FILE", "rest/data_json.php");
